f9c645e00f9be32d666c1fa4dde8663257868615
pavan123b <pavankumar0167@Pavans-MacBook-Air.local>
2024-06-26 13:19:59 +0530

    created pool

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ const express= require("express");
 const app = express();
 const cors= require("cors");
 
+
+const pool = require("./db");
+
+
 app.use(cors());
 app.use(express.json());
+
+
 
 app.listen(4000, ()=>{
     console.log("I'm listening");
