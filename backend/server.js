47f1f18e8dc764bc1d5a8efa17c99a6dec6ebd9f
pavan123b <pavankumar0167@Pavans-MacBook-Air.local>
2024-06-26 14:51:54 +0530

    made post req to add fav

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,31 @@ const cors= require("cors");
 
 const pool = require("./db");
 
-
+//midleware
 app.use(cors());
 app.use(express.json());
 
+//routes
+
+//show fav list
+
+//addfav
+app.post('/addfav',async (req,res)=>{
+    try {
+        const { pkgname, reason} = req.body;
+        const newfav = await pool.query("INSERT INTO favlist (pkgname,reason) VALUES($1,$2) RETURNING *", [pkgname,reason]);
+        console.log(req.body);
+        res.json(newfav);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+//edit fav reason
+
+//delete fav reason
+
+//
 
 
 app.listen(4000, ()=>{
